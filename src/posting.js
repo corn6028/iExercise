5b2971c9f0676d70fc69d4170fea2abbf3e822a5
corn6028 <100703005@nccu.edu.tw>
2015-01-07 23:55:06 +0800

    new

---
+++ src/posting.js
@@ -0,0 +1,17 @@
+function validate_required(field,alerttxt)
+{
+    with (field)
+    {
+    if (value==null||value=="")
+      {alert(alerttxt);return false}
+      else {return true}
+      }
+}
+function validate_form(thisform)
+{
+    with (thisform)
+    {
+    if (validate_required(distance,"Distance must be filled out!")==false)
+      {distance.focus();return false}
+    }
+}
